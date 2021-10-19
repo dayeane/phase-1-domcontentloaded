e7f618cf9436e80e921485e20a4fee30623a8515
Dayeane <dayeane85@gmail.com>
2021-10-18 17:36:16 -0700

    DOMContentLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+console.log("dfg")
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    // debugger;
+    document.querySelector("#text").textContent = "This is really cool!"
+
+
+
+    
+  });
+  console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
